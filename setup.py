076b5941d3d6136c6fab34b4627d86ac6fe0ec05
songbo327 <147338447+songbo327@users.noreply.github.com>
2024-04-20 19:15:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='file-renaming-tool',
     version='0.1.0',
     url='https://github.com/yourusername/file-renaming-tool',
-    author='Songbo gao',
+    author='Songbo gao; Lijie lin; Keyan Shen',
     author_email='songbog327@gmail.com',
     description='File rename',
     packages=find_packages(),    
